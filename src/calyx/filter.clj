7da730550fe5bd5a3c7347d7b227af9950ccefcb
Daniel Stephens <daniel.stephens02@gmail.com>
2020-01-03 17:20:19 +0000

    More filtering

--- src/calyx/filter.clj
+++ src/calyx/filter.clj
@@ -1,123 +1,111 @@
 (ns calyx.filter
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.zip :as zip]
+            [clojure.walk :as walk]))
+
+(defn spy [x]
+  (clojure.pprint/pprint x)
+  x)
 
 (def special-filter?
   (comp #{:and "and"
           :or "or"}
         first))
 
-(defn- make-arg
-  [arg]
-  {:id (first arg)
-   :value (second arg)})
-
 (defn- restructure-filter
-  [f]
-  {:or (concat [{:and (concat (->> f
-                                   (remove special-filter?)
-                                   (map make-arg))
-                              (->> f
-                                   (filter (comp #{:and "and"} first))
-                                   (map (comp restructure-filter second))))}]
-               (->> f
-                    (filter (comp #{:or "or"} first))
-                    (map (comp restructure-filter second))))})
-
-(defn- restructure-stack
-  [args]
-  (cond
-    (#{:and "and"
-       :or "or"} (first args)) {(keyword (first args)) (map restructure (second args))}
-
-    (= :filter (first args)) (restructure-filter (second args))
-
-    :else (make-arg args)))
+  [v]
+  {::type :or
+   ::children (concat [{::type :and
+                        ::children (concat (remove special-filter? v)
+                                           (->> v
+                                                (filter (comp #{:and "and"} first))
+                                                (map (comp restructure-filter second))))}]
+                      (->> v
+                           (filter (comp #{:or "or"} first))
+                           (map (comp restructure-filter second))))})
 
 (defn- restructure
   [args]
-  {:and (map restructure-stack args)})
+  (zip/zipper
+    ::type
+    ::children
+    (fn [node children] (assoc node ::children children))
+    {::type :and
+     ::children (map (fn [[k v :as f]]
+                       (if (= :filter k)
+                         (restructure-filter v)
+                         f))
+                     args)}))
 
 (defn- arg-type
   [arg]
-  (cond
-    (:and arg) :and
-    (:or arg) :or
-    :else :arg))
+  (or (::type arg)
+      :simple))
 
-(defn- simplify-noops
-  [m]
-  (cond
-    (:id m) [m]
-    (< (count (some m [:and :or])) 2) (mapcat simplify-noops (some m [:and :or]))
-    :else [{(arg-type m) (mapcat simplify-noops (some m [:and :or]))}]))
+(defn postwalk [f loc]
+  (let [loc (if-some [loc (zip/down loc)]
+              (loop [loc loc]
+                (let [loc (postwalk f loc)]
+                  (if-some [loc (zip/right loc)]
+                    (recur loc)
+                    (zip/up loc))))
+              loc)]
+    (f loc)))
 
-(defn- flatten-ops
-  [m]
-  (let [t (arg-type m)]
-    (if (= :arg t)
-      [m]
-      [{t (mapcat (fn [n]
-                    (let [nt (arg-type n)]
-                      (cond
-                        ;; If same type then raise things up a level, i.e. get rid of the op
-                        (= t nt) (mapcat flatten-ops (get n nt))
-                        ;; If this is a different op then keep it but simplify down the branch
-                        (#{:and :or} nt) [{nt (mapcat flatten-ops (get n nt))}]
-                        ;; If this is a simple arg then just simplify it directly
-                        :else (flatten-ops n))))
-                  (get m t))}])))
+(defn- simplify-children
+  [branch-node children]
+  (let [t (arg-type branch-node)
+        children-of-type (fn [t] (filter #(t %) children))
 
-(defn combine-ops [m]
-  (let [t (arg-type m)]
-    (if (= :arg t)
-      [m]
-      (let [{:keys [arg
-                    and
-                    or]} (group-by arg-type (get m t))
-            n (concat (apply concat arg)
-                      (when (seq and) [{:and (apply concat (map :and and))}])
-                      (when (seq or) [{:or (apply concat (map :or or))}]))
-            _ (println "b" m)
-            _ (calyx.core/spy (mapcat combine-ops n))
-            _ (println n)]
-        [{t (mapcat combine-ops n)}]))))
+        squashable #(or (= t (arg-type %))
+                        (and (::type %)
+                             (-> % ::children count (< 2))))
+        to-squash (children-of-type squashable)
+        others (children-of-type (complement squashable))
+
+        new-children (concat others
+                             (mapcat ::children to-squash))]
+    (if (= children new-children)
+      new-children
+      (recur branch-node new-children))))
 
 (defn- simplify
   [m]
-  (println "a")
-  (->> m
-       combine-ops
-       first
-       calyx.core/spy
-       simplify-noops
-       first
-       calyx.core/spy
-       flatten-ops
-       first
-       calyx.core/spy
-       ))
+  (let [z (zip/root (postwalk
+                      (fn [loc]
+                        (if (zip/branch? loc)
+                          (let [children (zip/children loc)
+                                node (zip/node loc)]
+                            (zip/replace loc (assoc (zip/node loc)
+                                               ::children (simplify-children node children))))
+                          loc))
+                      m))]
+    (case (count (::children z))
+      0 nil
+      1 (-> z ::children first)
+      z)))
 
-(defn sqlise [schema ctx args]
+(defn sqlise [schema ctx m]
   (cond
-    (or (:and args)
-        (:or args))
-    (let [and? (:and args)
-          join (if and? " AND " " OR ")
-          parts (remove nil? (map (partial sqlise schema ctx) (some args [:and :or])))]
+    (nil? m) nil
+
+    (::type m)
+    (let [join (if (= :and (::type m)) " AND " " OR ")
+          parts (remove nil? (map (partial sqlise schema ctx) (::children m)))]
       [(str "("
             (str/join join (map first parts))
             ")")
        (mapcat second parts)])
 
-    (string? (get schema (first args))) [(get schema (first args)) [(second args)]]
+    (string? (get schema (first m))) [(get schema (first m)) [(second m)]]
 
-    (fn? (get schema (first args))) ((get schema (first args)) ctx (second args))))
+    (fn? (get schema (first m))) ((get schema (first m)) ctx (second m))))
 
 (defn where-fn [schema]
   (fn [ctx arg-v]
     (->> arg-v
          restructure
-         ;calyx.core/spy
+         ;spy
          simplify
-         calyx.core/spy
+         ;spy
          (sqlise schema ctx))))
